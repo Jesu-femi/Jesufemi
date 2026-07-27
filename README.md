73d35077a80c9f016c49f0ffa170b830c68e1423
sufficient-lash <105395836+sufficient-lash@users.noreply.github.com>
2026-07-27 10:51:04 +0100

    the back to top botton and email set up

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Jesufemi
+
+# https://www.linkedin.com/jobs/collections/recommended/?currentJobId=4283348645&discover=recommended&discoveryOrigin=JOBS_HOME_JYMBII
+
+# https://github.com/Jesu-femi/Jesufemi
+
+# For the emailjs.com
+# For the scroll reveal on cdnjs.com scrollreveal
--- app.js
+++ app.js
@@ -48,6 +48,51 @@ const contactForm = document.getElementById("contact-form"),
 const sendEmail = (e) => {
 	e.prventDefault();
 	/* ServiceID - TemplateID - #form - PublicKey */
-    
+	emailjs.sendForm("", "", "").then(
+		() => {
+			contactmessage.textContent = "Message sent successfully";
+
+			setTimeout(() => {
+				contactmessage.textContent = "";
+			}, 5000);
+
+			contactForm.reset();
+		},
+		() => {
+			// Error message
+			contactmessage.textContent = "Message NOT sent (Serbice error)";
+		},
+	);
 };
 contactForm.addEventListener("submit", sendEmail);
+
+/*============== Show Scroll UP ========== */
+const scrollUp = () => {
+	const scrollUp = document.getElementById("scroll-up");
+	this.scrollY >= 350
+		? scrollUp.classList.add("show-scroll")
+		: scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*============== Scroll Sectoin Action Link========== */
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+	const scrollDown = window.scrollY;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight,
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute("id"),
+			sectionClass = document.querySelector(".nav-menu a[href*='+ sectionId +']");
+
+		if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+			sectionClass.classList.add("active-link");
+		} else {
+			sectionClass.classList.remove("active-link");
+		}
+	});
+};
+
+window.addEventListener("scroll", scrollActive);
